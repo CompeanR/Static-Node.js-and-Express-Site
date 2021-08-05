c8b06601ac54b0c20c88732bea97c9e094df9824
Compean <javier.copmeanr@gmail.com>
2021-08-04 19:16:34 -0500

    organization folder

--- app.js
+++ app.js
@@ -14,20 +14,20 @@ app.use('/about', aboutRoute);
 
 app.use((req, res, next) => {
   console.log('Oops!  It looks like something went wrong on the server.')
-    res.status(404).render('page-not-found');
+  res.status(404).render('page-not-found');
 });
 
 app.use((err, req, res, next) => {
 
-    if (err.status === 404) {
-      console.log('Oops!  It looks like something went wrong on the server.')
-      res.status(404).render('page-not-found', { err });
-    } else {
-      err.message = err.message || `Oops!  It looks like something went wrong on the server.`;
-      res.status(err.status || 500).render('error', { err });
-    };
+  if (err.status === 404) {
+    console.log('Oops!  It looks like something went wrong on the server.')
+    res.status(404).render('page-not-found', { err });
+  } else {
+    err.message = err.message || `Oops!  It looks like something went wrong on the server.`;
+    res.status(err.status || 500).render('error', { err });
+  };
 });
 
 app.listen(3000, () => {
-    console.log('listen on port 3000');
+  console.log('listen on port 3000');
 });
